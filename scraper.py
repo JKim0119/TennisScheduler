c06c54cd9f3a6c2884d24f378affef643efe442a
Jacob Kim <jacob.yeonwoo@gmail.com>
2019-07-06 02:33:16 -0400

    Created parser

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import bs4
 from bs4 import BeautifulSoup
 import requests
 
@@ -12,20 +13,49 @@ def current_tourneys():
 
 	for line in page_content.find_all('a', href=True, attrs={'data-ga-label' : "Schedule"}):
 		curr.append(line.attrs['href'])
-
+	
 	return curr
 
 def players(tourney):
 	page_link = atp + tourney
 	page_response = requests.get(page_link, timeout=5)
 	page_content = BeautifulSoup(page_response.content, "html.parser")
-	
-	with open("schedule.txt", "w") as file:
-		file.write(str(page_content))
+
+	schedules = page_content.find_all('div', attrs={'class' : 'sectioned-day-tables'})[0]
+
+	listOfCourts = schedules.find_all('span', attrs={'class' : 'day-table-heading'})
+	#print(listOfCourts)
+
+	for table in schedules.find_all(attrs={'class' : 'day-table'}):
+		court = table.find(attrs={'class' : 'day-table-heading'}).contents[0]
+		time = table.find(attrs={'colspan' : '10'}).contents[2].strip()
+		
+		print(court)
+		print(table.find(attrs={'colspan' : '10'}).contents[2].strip())
+
+		for match in table.find_all(attrs={'class' : 'day-table-name'}):
+			for cont in match.contents:
+				if(cont != '\n'):
+					if(type(cont) is bs4.element.Tag):
+						if(cont.has_attr('data-ga-label')):
+							print(cont.attrs['data-ga-label'])
+						else:
+							for player in cont.find_all('a'):
+								print(player.attrs['data-ga-label'])
+					else:
+						print(cont.strip())
+						print(cont.strip())
+		print("")
+
+
+
+	with open("schedule2.txt", "w") as file:
+		for line in page_content.find_all('div', attrs={'class' : 'sectioned-day-tables'}):
+			file.write(str(line))
 
 
 if __name__ == '__main__':
 	tourneyList = current_tourneys()
-	print(tourneyList)
+	#print(tourneyList)
 	for eachTourney in tourneyList:
 		players(eachTourney)
